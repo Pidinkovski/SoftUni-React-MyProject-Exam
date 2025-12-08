9b1c947791d897668e8a1a10f3951309e687332a
Pidinkovski <pdinkovski@abv.bg>
2025-12-08 17:56:03 +0200

    create a simple footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header />
+    <Footer/>
     </>
   )
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div>
+            <footer className='footer'>
+                <p>2025 HealthyLife. Stay strong. Stay focused.</p>
+                <p>Built with React by <strong>Pavel Dinkovski</strong></p>
+            </footer>
+        </div>
+    )
+}
