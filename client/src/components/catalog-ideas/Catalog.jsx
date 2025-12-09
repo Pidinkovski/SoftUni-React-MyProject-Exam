08f23038e5ac9235ffd2a1ada11c7fd89c6d6def
Pidinkovski <pdinkovski@abv.bg>
2025-12-09 21:19:27 +0200

    add some fixed categories , with simple css

--- client/src/components/catalog-ideas/Catalog.jsx
+++ client/src/components/catalog-ideas/Catalog.jsx
@@ -1,12 +1,31 @@
 import './catalog.css';
+
+const categories = [
+    { categoryType: 'workouts', categoryAbout: 'Workouts', imageUrl: '/images/categoryWorkout.jpg' },
+    { categoryType: 'lifestyle', categoryAbout: 'Lifestyle', imageUrl: '/images/healthyLifeStyle.jpg' },
+    { categoryType: 'food', categoryAbout: 'Food', imageUrl: '/images/healthyFood.jpg' },
+    { categoryType: 'mindfull', categoryAbout: 'Mindful Set', imageUrl: '/images/mindfulSet.png' },
+]
 export default function Catalog() {
     return (
         <section className="catalog-ideas">
             <h2 className="catalog-title">Just choose your category , and enjoy others people have shared and tried</h2>
 
-            <ul className="catalog-ideas-list">
-                <li className="catalog-idea-item"></li>
-            </ul>
+            <div className='catalog-ideas-scroller'>
+                <ul className="catalog-ideas-list">
+                    {categories.map((category) => (
+                        <li
+                            key={category.categoryType}
+                            className="catalog-idea-item">
+                            <div className="catalog-idea-image-wrapper">
+                                <img src={category.imageUrl} alt={category.categoryType} className="catalog-idea-image" />
+                            </div>
+                            {category.categoryAbout}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <p className='catalog-last-text'>DONT FORGET if you have something that you think worked for your and makes you feel better , it will be pleasure to share with us.</p>
         </section>
     )
 }
