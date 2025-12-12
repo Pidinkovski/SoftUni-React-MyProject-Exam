9258fa990ee7d7563ced8b420412606064c1fa6f
Pidinkovski <pdinkovski@abv.bg>
2025-12-12 21:41:02 +0200

    full redo on home logic , because of issue

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,37 +1,58 @@
-import useFetchOnMount from '../../hooks/useFetchOnMount'
 import './home.css'
+
+import useFetchOnMount from '../../hooks/useFetchOnMount';
 
 const BASE_URL = 'http://localhost:3030'
 
 export default function Home() {
 
-    const { currentData } = useFetchOnMount(`${BASE_URL}/data/ideas`, [])
+  const searchPart = encodeURIComponent('idea=ideaId:ideas')
 
-    // currentData.sort((a, b) => b.likes.length - a.likes.length || b.title.localeCompare(a.title))
-    // const popularIdeas = currentData.slice(0, 3)
+  const { currentData } = useFetchOnMount(`${BASE_URL}/data/likes?load=${searchPart}`, [])
+
+  function getIdeasCount(likes) {
+    const ideasWithProps = new Map()
+
+    for (const like of likes) {
+      const ideaId = like.ideaId;
+      const title = like.idea?.title;
+
+      if (!ideasWithProps.has(ideaId)) {
+        ideasWithProps.set(ideaId, { ideaId, count: 0, title });
+      }
 
 
-    return (
-        <section className="home">
-            <div className="home-header-title">
-                <h1>Welcome to Healty Lifestyle</h1>
-            </div>
+      ideasWithProps.get(ideaId).count += 1;
+    }
 
-            <div className='home-center-text'>
-                <p>If you want to improve your overall health and lifestyle, you are on the right place.</p>
-                <p>Here you can find ideas about simple or advance workouts, lifestyle tips , how to stay healty and some delicious food recipies</p>
-                <p>Also if you have something helpfull in mind , you can always share with us.</p>
-            </div>
-            <div className="home-popular">
-                <p className='home-popular-title'>Here you can find also the most liked ideas : </p>
-                {/* <ul className="home-popular-list">
-                    {popularIdeas?.map(idea => <li key={idea?._id} className="home-popular-item">
-                        <h3 className='home-popular-idea-title'>{idea?.title}</h3>
-                        <p className='home-popular-idea-likes'>Likes : {idea.likes?.length}</p>
-                    </li>)}
+    return [...ideasWithProps.values()]
+      .sort((a, b) => b.count - a.count || a.title.localeCompare(b.title))
+      .slice(0, 3);
+  }
 
-                </ul> */}
-            </div>
-        </section>
-    )
+  const populizedIdeas = getIdeasCount(currentData);
+
+  return (
+    <section className="home">
+      <div className="home-header-title">
+        <h1>Welcome to Healty Lifestyle</h1>
+      </div>
+
+      <div className='home-center-text'>
+        <p>If you want to improve your overall health and lifestyle, you are on the right place.</p>
+        <p>Here you can find ideas about simple or advance workouts, lifestyle tips , how to stay healty and some delicious food recipies</p>
+        <p>Also if you have something helpfull in mind , you can always share with us.</p>
+      </div>
+      <div className="home-popular">
+        <p className='home-popular-title'>Here you can find also the most liked ideas : </p>
+        <ul className="home-popular-list">
+          {populizedIdeas?.map(idea => <li key={idea?.ideaId} className="home-popular-item">
+            <h3 className='home-popular-idea-title'>{idea?.title}</h3>
+            <p className='home-popular-idea-likes'>Likes : {idea.count}</p>
+          </li>)}
+
+        </ul>
+      </div>
+    </section>
+  )
 }
