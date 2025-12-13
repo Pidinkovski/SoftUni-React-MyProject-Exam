e4ff4e29ae9b571801a249badd06acad3c4cf982
Pidinkovski <pdinkovski@abv.bg>
2025-12-14 00:05:08 +0200

    add welcome text

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,22 +5,29 @@ import UserContext from '../../contexts/UserContext';
 
 export default function Header() {
 
-    const { isAuthenticated } = useContext(UserContext);
+    const { isAuthenticated, user } = useContext(UserContext);
 
     return (
 
         <nav className='header-nav'>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/ideas">Catalog</NavLink>
-            {isAuthenticated
-                ? (<>
-                    <NavLink to="/create">Create</NavLink>
-                    <NavLink to="/logout">Logout</NavLink>
-                </>)
-                : (<>
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/register">Register</NavLink>
-                </>)}
+            <div className="nav-links">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/ideas">Catalog</NavLink>
+                {isAuthenticated
+                    ? (<>
+                        <NavLink to="/create">Create</NavLink>
+                        <NavLink to="/logout">Logout</NavLink>
+                    </>)
+                    : (<>
+                        <NavLink to="/login">Login</NavLink>
+                        <NavLink to="/register">Register</NavLink>
+                    </>)}
+            </div>
+            {isAuthenticated && user.email &&
+                <span className="hello-user">
+                    Hello, {user.email}
+                </span>}
         </nav>
+
     )
 }
